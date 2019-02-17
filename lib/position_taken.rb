95f7445ad66fee472fa131d53748c9e9fee60756
DeChaune Myers <dmyers725@gmail.com>
2019-02-17 18:13:45 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,3 @@
-# board is an array of strings
-# ["X", ""]
-#index is a number to check
-# it could be 0, 4, 7, 1 
-
 def position_taken?(board, index)
  position = board[index]
  if position == "" || position == " " || position == nil
