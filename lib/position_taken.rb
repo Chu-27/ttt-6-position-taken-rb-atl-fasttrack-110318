410a5071cbdcb6d5405e2f6890fcb612a674940c
DeChaune Myers <dmyers725@gmail.com>
2019-02-17 17:40:02 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,10 +1,15 @@
+# board is an array of strings
+# ["X", ""]
+#index is a number to check
+# it could be 0, 4, 7, 1 
+
 def position_taken?(board, index)
- puts board.inspect
+ puts board[0]
  puts index
  if board[index] == " "
-   return false
+   return 
  else 
- if board[index] == "x" || "o"
-   return true
+ if board[index] == "x" || "0"
+   return 
 
 
